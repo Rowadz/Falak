4c904bdf4f95c6efdb0a2ff693bc14f2717d02de
MohammedAl-rowad <rowadwo@gmail.com>
2020-12-18 15:18:15 +0200

    Remove unnecessary code #1

--- falak-api/src/app.controller.ts
+++ falak-api/src/app.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get, PlainLiteralObject } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): PlainLiteralObject {
-    return { mes: 'w' };
-  }
 }
