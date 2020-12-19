05c5830c91721640faf4388987d9ac1fd03e3d08
MohammedAl-rowad <rowadwo@gmail.com>
2020-12-19 03:32:31 +0200

    Add socket io to react #3

--- falak-react/src/App.tsx
+++ falak-react/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
 import { Button } from '@chakra-ui/react'
 import { useColorMode } from '@chakra-ui/react'
+import { connect } from 'socket.io-client'
+// TODO:: make this link dynamic and reads the port from the backend 
+const socket = connect('http://localhost:3030', { transports: ['websocket'] })
+
+socket.on('events', (data: unknown) => {
+  console.log('event', data) // TODO:: add data to redux from here (in some way)
+})
 
 const App = () => {
   const { toggleColorMode } = useColorMode()
-
   return (
     <Button colorScheme="blue" onClick={toggleColorMode}>
       Button
